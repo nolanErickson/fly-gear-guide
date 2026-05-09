fcfa7f2b07dfbbb2681f65e15c7b2a3dca23d8d1
nolanErickson <nrerickson2016@gmail.com>
2026-05-08 21:23:55 -0400

    Some core files added for objects, repo, and services

---
+++ Models/Gear.cs
@@ -0,0 +1,11 @@
+public abstract class Gear
+{
+  public string Name { get; set; }
+
+  public string Price { get; set; }
+
+  public string Weight { get; set; }
+
+  public string Link { get; set; }
+
+}
